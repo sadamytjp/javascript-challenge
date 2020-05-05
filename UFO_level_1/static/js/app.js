f5f46a789fcae929b517a6fd21a46a7bab95a998
Juan Pablo Sada <sadamytjp@gmail.com>
2020-05-05 10:30:40 -0500

    listo

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -1,4 +1,30 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+function createTable(data) {
+    tbody.html("");
+    data.forEach((busqueda) => {
+        var row = tbody.append("tr")
+    Object.entries(busqueda).forEach((value) => {
+        var cell = row.append("td");
+        cell.text(value);
+        }); 
+    });
+};
+
+function filtro() {
+    var fecha = d3.select("#datetime").property("value");
+    console.log(fecha);
+    var dataFiltrada = tableData;
+    console.log(dataFiltrada);
+    if (fecha){
+        dataFiltrada = dataFiltrada.filter(row => row.datetime === fecha);
+        console.log(dataFiltrada);
+    }
+    createTable(dataFiltrada)
+}
+d3.selectAll("#filter-btn").on("click", filtro)
+
--- UFO_level_2/static/js/app.js
+++ UFO_level_2/static/js/app.js
@@ -1,4 +1,46 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// from data.js
+var tableData = data;
+console.log(tableData);
+
+// Esta funcion crea la tabla en la página
+var tbody = d3.select("tbody");
+function createTable(data) {
+    tbody.html(" ");
+    data.forEach((busqueda) => {
+        var row = tbody.append("tr")
+        Object.entries(busqueda).forEach((value) => {
+        var cell = row.append("td");
+        cell.text(value);
+        }); 
+    });
+};
+
+
+
+function filtro() {
+    event.preventDefault();
+    var fecha = d3.select("#fecha").property("value");
+    var ciudad = d3.select("#ciudad").property("value");
+    var estado = d3.select("#estado").property("value");
+    var pais = d3.select("#pais").property("value");
+    var forma = d3.select("#forma").property("value");
+    console.log(fecha, ciudad, estado, pais, forma);
+    var dataFiltrada = tableData;
+    console.log(dataFiltrada);
+    if (fecha){
+        var dataFiltrada = tableData.filter(row =>
+            row.datetime === fecha &&
+            row.city === ciudad &&
+            row.state === estado &&
+            row.country === pais &&
+            row.shape === forma
+        );
+        console.log(dataFiltrada);
+    }
+    createTable(dataFiltrada)
+}
+d3.selectAll("#filter-btn").on("click", filtro)
+
